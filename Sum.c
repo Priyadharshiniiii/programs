80870b32ce78f09729ce432c2a789a6c90058768
Priyadharshiniiii <94179842+Priyadharshiniiii@users.noreply.github.com>
2021-11-23 13:05:26 +0530

    Create Sum.c

---
+++ Sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int sum(int n);
+{
+if(n!=0)
+{
+return(n%10+sum(n/10));
+}
+else
+{
+return 0;
+}
+}
+int main() 
+{
+int n;
+printf("Enter the number:");
+scanf("%d",&n);
+printf("Sum of digits of %d is %d \n",n,sum(n));
+return 0;
+} 
